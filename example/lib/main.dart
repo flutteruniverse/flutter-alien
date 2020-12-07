3f9289e1a5f1cdb8ed0b738f6c07e989422abbaa
Matias de Andrea <deandreamatias@gmail.com>
2020-12-07 13:01:28 +0100

    feat: Create example

---
+++ example/lib/main.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_alien/flutter_alien.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final mediumFeedService = MediumService();
+
+  @override
+  void initState() {
+    mediumFeedService.initService();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Example get data'),
+      ),
+      body: FutureBuilder<MediumChannel>(
+        future: mediumFeedService.getMediumFlutterFeed(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    snapshot.data.title,
+                  ),
+                  Text(
+                    snapshot.data.description,
+                  ),
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: snapshot.data.mediumPosts.length,
+                      itemBuilder: (context, index) => Card(
+                        margin: const EdgeInsets.all(8.0),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            children: [
+                              Text(snapshot.data.mediumPosts[index].title),
+                              Text(snapshot.data.mediumPosts[index].link),
+                              Text(snapshot.data.mediumPosts[index].pubDate),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            );
+          }
+          return Text('Loading data');
+        },
+      ),
+    );
+  }
+}
