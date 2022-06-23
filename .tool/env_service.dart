5af5ae2744b19cd549c1d3547d787752e1d5cefb
Matias de Andrea <deandreamatias@gmail.com>
2022-06-23 18:24:37 +0200

    refactor: Change variable name

--- .tool/env_service.dart
+++ .tool/env_service.dart
@@ -6,8 +6,8 @@ Future<void> main() async {
     'githubToken': Platform.environment['GITHUB_TOKEN'],
   };
 
-  final filename = 'lib/src/config/env.dart';
-  await File(filename).writeAsString('''/// Global enviroment variables\n
+  final envName = 'lib/src/config/env.dart';
+  await File(envName).writeAsString('''/// Global enviroment variables\n
       const Map<String, String> environment =
       <String, String>${json.encode(config)};''');
 }
